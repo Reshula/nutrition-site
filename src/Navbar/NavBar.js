b6ad4aeeb4422c487909f64f97c8fed2152ce53a
Reshula <reyhana328@gmail.com>
2024-05-13 11:41:25 -0400

    was modified in NavBar component

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -1,17 +1,17 @@
-import { useState , useEffect} from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
-import {  IoMenu } from "react-icons/io5";
+// 
+import { useState , useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import {  IoMenu} from "react-icons/io5";
 import './NavBar.css';
-import logo from '../Assets/logo_avocado.png';
+import logo from '../Assets/logo_avocado.png'
 
+const NavBar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
 
-const NavBar = () =>{
-
-  const [showNavbar, setShowNavbar] = useState(false);
-  const toggleShowNavbar = () => {
+  const handleShowNavbar = () => {
     setShowNavbar(!showNavbar)
   }
- 
+
   const [fixNav, setFixNav] =useState(false);
   const setFixNavbar = () => {
     if (window.scrollY > 0 ) {
@@ -24,41 +24,45 @@ const NavBar = () =>{
     return () => window.removeEventListener("scroll", setFixNavbar);
   }, []);
 
-return (
-  <div  className={fixNav ? 'header fixed' : 'header'} >
-  <nav className="navbar">
-    <div className="container">
-      <div className="logo">
-      
-      </div>
-      <div className="menu-icon" onClick={toggleShowNavbar}>
-        <IoMenu />
-      </div>
-      <div className={`nav-elements  ${showNavbar && 'active'}`}>
-        <ul>
+  return (
+    <div className={fixNav ? 'header fixed' : 'header'}>
+    <nav className="navbar">
+     
+       
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <IoMenu />
+        </div>
+     
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul className='ul-nav'>
+            <li>
+              <NavLink to="/"> <img className='logo-nav' src={logo}alt='logo'/></NavLink>
+            </li>
+            <li>
+              <NavLink to="/program_section">Программы</NavLink>
+            </li>
+           
+            <li>
+              <NavLink to="/nutritionCal_section">Калькулятор</NavLink>
+            </li>
+            <li>
+              <NavLink to="/consultation_section">Консультации</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about_section">Обо мне</NavLink>
+            </li>
           
-          <li>
-          <Link to="/"className='menu-list'><img className='logo-avacado' src={logo} alt='avacado'/></Link>
-          </li>
-          <li>
-          <Link to="/program_section"className='menu-list'>Программы </Link>
-          </li>
-          <li>
-            
-            <Link to="/nutritionCal_section"className='menu-list'>Калькулятор </Link>
-          </li>
-          <li>
-          <Link to="/consultation_section"className='menu-list'>Консультации</Link>
-          </li>
-          <li>
-       <Link to="/about_section"className='menu-list'>Обо мне</Link>
-       </li>
-          
-        </ul>
-      </div>
+          </ul>
+        </div>
+     
+    </nav>
     </div>
-  </nav>
-  </div>
-)
+  )
 }
+
 export default NavBar;
+
+
+
+
+
